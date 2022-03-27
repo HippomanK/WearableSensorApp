b6f3e14ddb79c9d5fef5bbf76774ca1c6559d6b3
CheickTT <tourecheick291@gmail.com>
2022-03-27 13:27:08 -0500

    Python module added Note: might need to install Chaquopy.

--- app/src/main/java/no/nordicsemi/android/blinky/Graph.java
+++ app/src/main/java/no/nordicsemi/android/blinky/Graph.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -22,5 +25,18 @@ public class Graph extends AppCompatActivity {
 
         graphView = findViewById(R.id.graph);
         data = new LineGraphSeries<>(new DataPoint[]{});
+
+        //Start python
+        if(!Python.isStarted()){
+            Python.start(new AndroidPlatform(getApplicationContext()));
+        }
+        Python py = Python.getInstance();
+        //Access the ml module
+        PyObject module = py.getModule("test");
+        //get the predictions from the ml module
+        PyObject Prediction = module.callAttr("evaluate",data);
+        //debug the ml module
+        System.out.println(Prediction);
+
     }
 }
